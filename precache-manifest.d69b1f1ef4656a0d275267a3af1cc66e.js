f2fa37d38a413a163e96cfbbd66a6fb2bcdbcc1c
sieugene <gagarin126rus@Mail.ru>
2020-05-27 16:24:27 +0300

    Updates

---
+++ precache-manifest.d69b1f1ef4656a0d275267a3af1cc66e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1a7cd7243f26fa497a7e8ab08f36bc",
+    "url": "/ElfsightInc/index.html"
+  },
+  {
+    "revision": "a3bba64ebf304224a227",
+    "url": "/ElfsightInc/static/css/main.4041cb72.chunk.css"
+  },
+  {
+    "revision": "c2b6cf5b4e0edeaca2dc",
+    "url": "/ElfsightInc/static/js/2.73bfc464.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ElfsightInc/static/js/2.73bfc464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3bba64ebf304224a227",
+    "url": "/ElfsightInc/static/js/main.7b3ce672.chunk.js"
+  },
+  {
+    "revision": "0fc60e691fb6057d2feb",
+    "url": "/ElfsightInc/static/js/runtime-main.00e2a4bd.js"
+  },
+  {
+    "revision": "dfd2683c9701642c776e31d3b0d603a9",
+    "url": "/ElfsightInc/static/media/preload.dfd2683c.gif"
+  }
+]);
